95183e7133a3d6aef1ee2ca6f4996de2703b1344
Misha Sydoruk <sydoruk.m@gmail.com>
2026-02-28 21:14:22 +0200

    Remove MetalReleaseTracker.Benchmarks

--- src/MetalReleaseTracker.Benchmarks/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using BenchmarkDotNet.Running;
-using MetalReleaseTracker.Benchmarks.CatalogSyncService;
-using MetalReleaseTracker.Benchmarks.CoreDataService;
-using MetalReleaseTracker.Benchmarks.ParserService;
-
-BenchmarkRunner.Run<AlbumProcessedEventConsumerBenchmarks>();
